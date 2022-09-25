a7efab7875f58969ec5ec0a49ea0192956319c90
Tobi <bismarktobi@yahoo.com>
2022-09-25 05:00:09 -0500

    strings and numbers

---
+++ strings_and_numbers/numbers_arithmetic_operations.py
@@ -0,0 +1,5 @@
+#arithmetic operations
+#exmple: code to calculate how many munites there are in 20days.
+#20days, 24hours, 60minutes
+
+print(20 * 24 * 60)
---
+++ strings_and_numbers/strings_and_data_types.py
@@ -0,0 +1,6 @@
+# data types
+#strings(str) , number(int) , float(3.5)
+
+print("200 is a great number")
+print(3.5)
+print(3)
